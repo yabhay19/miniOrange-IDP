94d9671b60e413914773df7fd21f568f82b0c198
synprash <prashant.kabade@syngenta.com>
2018-03-19 10:26:17 +0530

    Applied two patches to mini_orange module

--- includes/metadata/metadata.php
+++ includes/metadata/metadata.php
@@ -1,11 +1,11 @@
 <?php
-define('DRUPAL_ROOT', dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))));
+define('DRUPAL_ROOT', dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))))));
 
 require_once DRUPAL_ROOT . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-  
-global $base_url;	
-$site_url = substr($base_url, 0, strpos($base_url, 'sites'));   
+
+global $base_url;
+$site_url = substr($base_url, 0, strpos($base_url, 'sites'));
 
 $entity_id = $site_url . '?q=admin/config/people/miniorange_saml_idp/';
 $login_url = $site_url . '?q=initiatelogon';
@@ -19,9 +19,9 @@ $certificate = str_replace( " ", "", $certificate );
 
 
 header('Content-Type: text/xml');
-echo'<?xml version="1.0" encoding="UTF-8"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="'.$entity_id.'"><md:IDPSSODescriptor WantAuthnRequestsSigned="false" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:KeyDescriptor 
+echo'<?xml version="1.0" encoding="UTF-8"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="'.$entity_id.'"><md:IDPSSODescriptor WantAuthnRequestsSigned="false" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:KeyDescriptor
 			use="signing">
-			<ds:KeyInfo 
+			<ds:KeyInfo
 				xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
 				<ds:X509Data>
 					<ds:X509Certificate>'.$certificate.'</ds:X509Certificate>
@@ -31,11 +31,11 @@ echo'<?xml version="1.0" encoding="UTF-8"?><md:EntityDescriptor xmlns:md="urn:oa
 
 		<md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress</md:NameIDFormat>
 		<md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified</md:NameIDFormat>
-		<md:SingleSignOnService 
-			Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" 
+		<md:SingleSignOnService
+			Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
 			Location="'.$login_url.'"/>
-		<md:SingleSignOnService 
-			Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" 
+		<md:SingleSignOnService
+			Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
 			Location="'.$login_url.'"/>
 	</md:IDPSSODescriptor>
 </md:EntityDescriptor>';
